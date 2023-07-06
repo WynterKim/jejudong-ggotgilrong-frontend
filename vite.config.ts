64e3481cee50da58c8d4df47f56116534e48bcfc
eden.kim <eden.kim@goorm.io>
2023-07-06 23:32:55 +0000

    build path changed

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/k92ebc3e719eda/[name]-[hash][extname]`;
+        },
     chunkFileNames: 'assets/k92ebc3e719eda/[name]-[hash].js',
     entryFileNames: 'assets/k92ebc3e719eda/[name]-[hash].js',
   },
